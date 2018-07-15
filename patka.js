0f04e34d8554afe92e2aa55dbe6769f930604e74
Mania <p.p.manka@gmail.com>
2018-07-15 23:49:25 +0200

    początek aplikacji

---
+++ patka.js
@@ -0,0 +1,27 @@
+﻿console.log("Sprawdź czy Twoja liczba jest większa czy mniejsza od 50!");
+const ask = require("readline-sync");
+
+var gramy = true;
+while (gramy) {
+    var numer = ask.question("Liczba mniejsza czy większa?").trim();
+    while ((isNaN(numer) || numer == "")) {
+        console.log("Podaj liczbę!");
+        numer = ask.question("> ").trim();
+    }
+
+    if (numer > 50) console.log("Liczba jest większa od 50");
+    else if (numer == 50) console.log("Liczba jest równa 50");
+    else if (numer < 50) console.log("Liczba jest mniejsza od 50");
+
+
+    const odpowiedz = ask.question("Chcesz grac dalej? <tak, nie>: ").trim().toLowerCase();
+    while (true) {
+        if (odpowiedz == "nie")
+            gramy = false;
+        else if (odpowiedz != "tak") {
+            console.log("Możesz wybrać tylko odpowiedź tak lub nie");
+            const odpowiedz = ask.question("Chcesz grac dalej? <tak, nie>: ").trim().toLowerCase();
+        }
+    }
+
+}
